2d7edcb79e5a058c6fe1160b623bef53adc51ca0
BonnetAntoine <109820079+BonnetAntoine@users.noreply.github.com>
2024-02-29 09:14:00 +0100

    Add files via upload

--- Programmation Objet/contact.php
+++ Programmation Objet/contact.php
@@ -1,11 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Formulaire de Contact</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<?php
     
+    $nom = $email = $message = "";
+    
+    // Vérifier si le formulaire a été soumis
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Récupérer les données du formulaire
+        $nom = test_input($_POST["nom"]);
+        $email = test_input($_POST["email"]);
+        $message = test_input($_POST["message"]);
+        
+        
+    }
+    
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+?>
+
+<h2>Formulaire de Contact</h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <label for="nom">Nom:</label>
+    <input type="text" id="nom" name="nom" required>
+    <br><br>
+    
+    <label for="email">E-mail:</label>
+    <input type="email" id="email" name="email" required>
+    <br><br>
+    
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" rows="4" required></textarea>
+    <br><br>
+    
+    <input type="submit" name="submit" value="Envoyer">
+</form>
+
+<?php
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo "<h2>Récapitulatif des informations:</h2>";
+        echo "Nom: " . $nom . "<br>";
+        echo "E-mail: " . $email . "<br>";
+        echo "Message: " . $message;
+    }
+?>
+
 </body>
 </html>
--- Programmation Objet/index.php
+++ Programmation Objet/index.php
@@ -23,45 +23,56 @@
             <h1>Activ'Actu</h1>
         </div>
         <div class="logo2">
-            <img src="#" alt="logo">
+            <img src="image/logo.png" alt="logo">
         </div>
     </div>
     <div>
-        <?php
-            $host = '127.0.0.1';
-            $db = 'actualités';
-            $user = 'root';
-            $pass = '';
-            $port = 3306;
-            $charset = 'utf8mb4';
+    <?php
+    $host = '127.0.0.1';
+    $db = 'actualités';
+    $user = 'root';
+    $pass = '';
+    $port = 3306;
+    $charset = 'utf8mb4';
 
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
 
-            try {
-                $pdo = new PDO($dsn, $user, $pass);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                $requete = "SELECT * FROM id_actualités"; 
-                $resultat = $pdo->query($requete);
+        $requete = "SELECT * FROM id_actualités ORDER BY date_publication DESC LIMIT 5"; 
+        $resultat = $pdo->query($requete);
 
-                while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<div class= paragraphe>";
-                    echo "<strong>Titre:</strong> {$ligne['titre']}<br>";
-                    echo "<strong>Corps:</strong> {$ligne['corps']}<br>";
-                    echo "<strong>Date de Publication:</strong> {$ligne['date_publication']}<br>";
-                    echo "<strong>Date de Révision:</strong> {$ligne['date_revision']}<br>";
-                    echo "<strong>Auteur:</strong> {$ligne['auteur']}<br>";
-                    echo "<strong>Tags:</strong> {$ligne['tags']}<br>";
-                    
-                    
-                    echo "<p><strong>Sources:</strong> {$ligne['sources']}</p>";
-                    
-                    echo "</div>";
-                }
-            } catch (PDOException $e) {
-                echo "Erreur de connexion à la base de données : " . $e->getMessage();
-            }
-        ?>
+        while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
+            echo "<div class=paragraphe style='color: #ffea98;'>";
+            echo "<strong>Titre:</strong> {$ligne['titre']}<br>";
+            echo "<strong>Corps:</strong> " . substr($ligne['corps'], 0, 100) . "... <br>";
+            echo "<a href='article1.php?id={$ligne['id']}'>Lire la suite</a><br>";
+            echo "<strong>Date de Publication:</strong> {$ligne['date_publication']}<br>";
+            echo "<strong>Date de Révision:</strong> {$ligne['date_revision']}<br>";
+            echo "<strong>Auteur:</strong> {$ligne['auteur']}<br>";
+            echo "<strong>Tags:</strong> {$ligne['tags']}<br>";
+
+            echo "<p><strong>Sources:</strong> {$ligne['sources']}</p>";
+            echo "<img src='{$ligne['image_url']}' alt='Image associée'>";
+
+            echo "</div>";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    }
+    ?>
     </div>
+
+    <footer>
+        <div class="footer-droite">
+            <p><a href="contact.php">Contact</a></p>
+        </div>
+        
+        <div class="footer-gauche">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.96777912509!2d2.2646344984437397!3d48.858825492072896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1709133114404!5m2!1sfr!2sfr" width="600" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </footer>
 </body>
 </html>
